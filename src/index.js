3312b093eb7c904420bd1d9a87b477b53bed8d47
Stanislau Virsky <vyr4376@yandex.ru>
2023-01-20 15:42:35 +0300

    feat: finish work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+ let concOne; 
+ let concTwo;
+ let concThree;
+ let concFour;
+ if (matrix == undefined || matrix.length == 0) {
+  return []
+ }
+ else if (matrix.length == 2) {
+  concOne = matrix[0].sort((a, b) => a - b);
+  concTwo = matrix[1].sort((a, b) => b - a);
+  return concOne.concat(concTwo);
+ }
+ else if (matrix.length == 3) {
+  concOne = matrix[0].sort((a, b) => a - b);
+  concTwo = matrix[1].sort((a, b) => b - a);
+  concThree = matrix[2].sort((a, b) => a - b)
+  return concOne.concat(concTwo, concThree)
+ }
+ else if (matrix.length == 4) {
+  concOne = matrix[0].sort((a, b) => a - b);
+  concTwo = matrix[1].sort((a, b) => b - a);
+  concThree = matrix[2].sort((a, b) => a - b);
+  concFour = matrix[3].sort ((a,b) => b - a);
+  return concOne.concat(concTwo, concThree, concFour)
+ }
 }
+ 
